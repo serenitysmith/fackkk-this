6c3872f1cffe76b55a08d134baf1766fded1fb51
Serenity smith <serenitysmithrn@hotmail.com>
2024-01-27 06:27:46 -0500

    //

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"; //Step 1
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"; //Step 2
+import Home from "./components/Home.js"; // Step 8
+import About from "./components/About.js";
+import Products from "./components/Products.js"; //NAV Example
+import Checkout from "./components/Checkout.js"; //NAV Example
+import ProductDetails from "./components/ProductDetails.js"; //Params 1
 
-function App() {
+export default function App() {
+  // Step 3
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    // Step 4
 
-export default App;
+    <Router>
+      {" "}
+      {/*Step 5*/}
+      <nav style={{ margin: 10 }}>
+        {" "}
+        {/*Step 11*/}
+        <Link to="/" style={{ padding: 5 }}>
+          {" "}
+          {/*Step 12*/}
+          Home
+        </Link>{" "}
+        {/*Step 12*/}
+        <Link to="/about" style={{ padding: 5 }}>
+          {" "}
+          {/*Step 13*/}
+          About
+        </Link>
+        {/*Step 13*/}
+        <Link to="/products" style={{ padding: 5 }}>
+          {" "}
+          {/*Step 13*/}
+          Products
+        </Link>
+        {/*Step 13*/}
+      </nav>{" "}
+      {/*Step 11*/}
+      <Routes>
+        {" "}
+        {/*Step 6*/}
+        <Route path="/" element={<Home />} /> {/*Step 7*/}
+        <Route path="/about" element={<About />} /> {/*Step 9*/}
+        <Route path="/products" element={<Products />} /> {/*NAV Example*/}
+        <Route path="/checkout" element={<Checkout />} /> {/*NAV Example*/}
+        <Route path="/products/:productId" element={<ProductDetails />} />{" "}
+        {/*Params 2*/}
+      </Routes>{" "}
+      {/*Step 6*/}
+    </Router> //Step 5
+  ); //Step 4
+} //Step 3
